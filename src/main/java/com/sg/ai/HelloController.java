0a2d79f2be947d8dd14f70fd4d067cbc49099bcb
310-0 <leo@shingu.ac.kr>
2025-10-14 12:10:18 +0900

    1st commit, Hellocontroller

---
+++ src/main/java/com/sg/ai/HelloController.java
@@ -0,0 +1,17 @@
+package com.sg.ai;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "안녕하세요, 이것은 이용희 교수가 2025년 10월14일 스프링으로 만든 페이지 입니다";
+	}
+	
+
+}

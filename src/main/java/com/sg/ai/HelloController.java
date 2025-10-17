52a6442a896d1846856b6b5109941fbf1ad699ad
310-0 <leo@shingu.ac.kr>
2025-10-17 10:30:35 +0900

    readme

--- src/main/java/com/sg/ai/HelloController.java
+++ src/main/java/com/sg/ai/HelloController.java
@@ -10,7 +10,8 @@ public class HelloController {
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
-		return "안녕하세요, 이것은 이용희 교수가 2025년 10월14일 스프링으로 만든 페이지 입니다";
+		return "안녕하세요, 이것은 이용희 교수가 2025년 10월14일 스프링으로 만든 페이지 입니다" +
+				"안녕하세요, 이것은 이용희 교수가 2025년 10월17일 스프링으로 만든 페이지 입니다";;
 	}
 	
 
